09a2e9ac87b4dd9d41fb12aac2024503a91d7ed9
leriklevchenko <leka200415@gmail.com>
2024-07-15 23:35:18 +0300

    Create homework4.py

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input('введите текст ')
+print('количество символов введённого текста:', len(my_string))
+print("Текст в верхнем регистре:", my_string.upper())
+print("Текст в нижнем регистре:", my_string.lower())
+print("Текст без пробелов:", my_string.replace(" ", ""))
+print("Первый символ строки:", my_string[0])
+print("Последний символ строки:", my_string[-1])
