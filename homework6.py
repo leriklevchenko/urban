d8751bb65c2cefab89fdd86cad4103313ca505f8
leriklevchenko <leka200415@gmail.com>
2024-07-18 21:07:37 +0300

    Create homework6.py

---
+++ homework6.py
@@ -0,0 +1,16 @@
+# 2
+my_dict = {'Дима': 2002, 'Катя': 2001, 'Настя': 2003}
+print (my_dict)
+print (my_dict ['Катя'])
+print (my_dict.get('Антон'))
+my_dict.update({'Эдуард': 2000, 'Глеб': 1999})
+del my_dict['Дима']
+print (my_dict)
+
+# 3
+my_set = {1, 2, 'a', 'b', 1, 'c', 2}
+print(my_set)
+my_set.add(3)
+my_set.add('d')
+my_set.remove('a')
+print(my_set)
